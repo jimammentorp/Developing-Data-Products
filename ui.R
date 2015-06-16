df7ef0309d6651a2c6bafa5d3f54785a9354a802
jimammentorp <jimayh@yahoo.com>
2015-06-16 12:43:49 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,18 +1,22 @@
 shinyUI(
   pageWithSidebar(
-    headerPanel("Predicted Winning Percentage and Wins"),
+    headerPanel("Predicted Baseball Winning Percentage and Wins"),
     sidebarPanel(
-      numericInput('scored', 'Runs Scored',1),
-      numericInput('allowed', 'Runs Allowed',1),
+      numericInput('scored', 'Enter the number of runs that have been scored in all games',1,
+                   min = 1, max = 200, step = 1),
+      numericInput('allowed', 'Enter the number of runs allowed in all games',1,
+                   min = 11, max = 200, step = 1),
       submitButton('Submit')
     ),
     mainPanel(
       h3('Results of prediction'),
-      h4('You entered'),
-      verbatimTextOutput("inputValue"),
+      h4('The total number of runs scored you entered are'),
+      verbatimTextOutput("inputValue1"),
+      h4('The total number of runs allowed you entered are'),
+      verbatimTextOutput("inputValue2"),
       h4('Which results in the predicted winning percentage of '),
       verbatimTextOutput("percent"),
-      h4('Which results in the predicted number of wins being '),
+      h4('Which results in the predicted number of wins over 162 games being '),
       verbatimTextOutput("wins")
     )
   )
